390d2a89191fff1a48f74e1c215c6bf1e31f7177
Ingrid <ib2@wellesley.edu>
2024-02-04 22:21:47 -0700

    Initial Commit

---
+++ IngridIntro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IngridIntro
+//
+//  Created by Ingrid Betancourt on 2/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
